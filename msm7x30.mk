65d64950091741cedc70b6f7416631267bd6176a
blk_jack <jcorcoran+github@gmail.com>
2013-05-28 19:37:50 -0400

    Set default composition to dyn, remove deprecated props

--- msm7x30.mk
+++ msm7x30.mk
@@ -136,6 +136,4 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.composition.type=gpu \
-    debug.sf.hw=1 \
-    debug.egl.hw=1
+    debug.composition.type=dyn
